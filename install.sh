2039f714655399f87219e4780a1228b39a10ead9
uav2 <augustoviniciud@gmail.com>
2023-11-01 08:19:27 -0300

    added instalation ROS2 in install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,19 @@
+sudo apt update && sudo apt install locales
+sudo locale-gen en_US en_US.UTF-8
+sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
+export LANG=en_US.UTF-8
+sudo apt install software-properties-common
+sudo add-apt-repository universe
+sudo apt update && sudo apt install curl -y
+sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
+sudo apt update && sudo apt upgrade -y
+sudo apt install ros-humble-desktop
+sudo apt install ros-dev-tools
+source /opt/ros/humble/setup.bash && echo "source /opt/ros/humble/setup.bash" >> .bashrc
+
+pip install --user -U empy pyros-genmsg setuptools
+
 sudo wget https://packages.osrfoundation.org/gazebo.gpg -O /usr/share/keyrings/pkgs-osrf-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/pkgs-osrf-archive-keyring.gpg] http://packages.osrfoundation.org/gazebo/ubuntu-stable $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/gazebo-stable.list > /dev/null
 sudo apt-get update
@@ -26,5 +42,4 @@ sudo rosdep init
 rosdep update
 rosdep install -i --from-path src --rosdistro humble
 colcon build
-
-source ~/laser_uav_system_ws/install/setup.bash && echo "source ~/laser_uav_system/install/setup.bash" >> .bashrc
+source ~/laser_uav_system_ws/install/setup.bash && echo "source ~/laser_uav_system_ws/install/setup.bash" >> .bashrc
