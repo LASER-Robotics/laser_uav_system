6bf4c3b1cd41d1c96fcbf9d282256445334cb17a
Augusto Vinicius <augustoviniciud@gmail.com>
2024-05-09 12:48:57 -0300

    fix repo for px4

--- install.sh
+++ install.sh
@@ -11,44 +11,38 @@ if ! ls "/opt/ros" | grep -q "humble"; then
   ./install_ros_humble.sh
 fi
 
-if ! ls "/usr/bin" | grep -q "gazebo"; then
-  ./install_gazebo.sh
-fi
+# if ! ls "/usr/bin" | grep -q "gazebo"; then
+#   # ./install_gazebo.sh
+# fi
 
 #Install dep and packages
-pip3 install kconfiglib
-pip3 install --upgrade gitman
+# pip3 install kconfiglib
+# pip3 install --upgrade gitman
 
-cd ~/git/laser_uav_system/.gitman/
-git clone git@github.com:PX4/PX4-Autopilot.git -b release/1.14 --recursive 
-cd PX4-Autopilot
-cd ~/git/laser_uav_system/ros_packages/
-ln -sf ~/git/laser_uav_system/.gitman/PX4-Autopilot ./
+# cd ../
+# gitman install
 
-cd ../
-gitman install
+# # Create workspace and create symbolic link for dirs
+# cd ~
+# mkdir laser_uav_system_ws
+# cd ~/laser_uav_system_ws
+# mkdir src
+# cd src
 
-# Create workspace and create symbolic link for dirs
-cd ~
-mkdir laser_uav_system_ws
-cd ~/laser_uav_system_ws
-mkdir src
-cd src
+# ln -sf ~/git/laser_uav_system/ros_packages/* ./
 
-ln -sf ~/git/laser_uav_system/ros_packages/* ./
-
-mkdir px4_packages
-mv micro_xrce_dds_agent ./px4_packages
-mv px4_msgs ./px4_packages
-mv PX4-Autopilot ./px4_packages
+# mkdir px4_packages
+# mv micro_xrce_dds_agent ./px4_packages
+# mv px4_msgs ./px4_packages
+# mv px4_firmware ./px4_packages
 
 # Set configs of PX4
-cd ~/git/laser_uav_system/ros_packages/PX4-Autopilot
+cd ~/git/laser_uav_system/ros_packages/px4_firmware
 bash ./Tools/setup/ubuntu.sh
 make px4_sitl
 
 # Make package with comunication protocol
-cd ~/laser_uav_system_ws/src/micro_xrce_dds_agent
+cd ~/git/laser_uav_system/ros_packages/micro_xrce_dds_agent
 mkdir build
 cd build
 cmake ..
