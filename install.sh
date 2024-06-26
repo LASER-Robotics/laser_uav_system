21d650515031091612f3f8f89bf97b737d8d05f1
Augusto Vinicius <augustoviniciud@gmail.com>
2024-06-26 17:10:03 -0300

    done v0.1

--- install.sh
+++ install.sh
@@ -56,7 +56,7 @@ cd ~/laser_uav_system_ws
 sudo rosdep init 
 rosdep update
 rosdep install -i --from-path src --rosdistro humble -y
-colcon build --symlink-install --packages-skip px4 microxrcedds_agent realsense2_camera 
+colcon build --symlink-install --packages-skip px4 microxrcedds_agent
 
 if [ $(grep -c "~/laser_uav_system_ws/install/setup.bash" ~/.bashrc) -ne 1 ]; then
   source ~/laser_uav_system_ws/install/setup.bash && echo -e "\n\n#source laser_uav_system workspace \nsource ~/laser_uav_system_ws/install/setup.bash" >> ~/.bashrc
