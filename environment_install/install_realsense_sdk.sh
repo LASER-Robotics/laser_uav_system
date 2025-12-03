49e113db86690958b06b3a8c8d1d074ed6de3479
Augusto Vinicius <augustoviniciud@gmail.com>
2025-12-03 00:59:44 -0300

    Update install_realsense_sdk.sh

--- environment_install/install_realsense_sdk.sh
+++ environment_install/install_realsense_sdk.sh
@@ -1,3 +1,9 @@
+if [ "${GITHUB_ACTIONS}" == "true" ]; then
+    BASE_DIR="$GITHUB_WORKSPACE"
+else
+    BASE_DIR="$(pwd)"
+fi
+
 sudo apt install ros-humble-diagnostic-updater
-sudo chmod +x ~/laser_uav_system_ws/src/laser_uav_drivers/ros_packages/realsense_drivers/scripts/libuvc_installation.sh
-~/laser_uav_system_ws/src/laser_uav_drivers/ros_packages/realsense_drivers/scripts/libuvc_installation.sh
+sudo chmod +x $BASE_DIR/laser_uav_system_ws/src/laser_uav_drivers/ros_packages/realsense_drivers/scripts/libuvc_installation.sh
+$BASE_DIR/laser_uav_system_ws/src/laser_uav_drivers/ros_packages/realsense_drivers/scripts/libuvc_installation.sh
